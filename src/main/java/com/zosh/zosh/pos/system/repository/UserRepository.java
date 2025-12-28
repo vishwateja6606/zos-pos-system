ede050a3beec7183e1b201676dcae6488457a1f7
vishwateja <vishwateja7777@gmail.com>
2025-12-28 16:18:12 +0530

    UserRepo changed

--- src/main/java/com/zosh/zosh/pos/system/repository/UserRepository.java
+++ src/main/java/com/zosh/zosh/pos/system/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.zosh.zosh.pos.system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zosh.zosh.pos.system.modal.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 	User findByEmail(String email);
 
